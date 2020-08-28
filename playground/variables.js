e3dfeb79a765bfd8c2f8dbdb6181b023f08b6707
yikth <netyth@gmail.com>
2020-08-28 17:16:12 +0800

    test javascript

---
+++ playground/variables.js
@@ -0,0 +1,14 @@
+console.log("Starting....");
+
+var c;
+
+console.log(c);
+
+var d = 55;
+
+// template literal `..`
+console.log(`My answer is ${d}`);
+
+var x = 10;
+var y = 20;
+console.log(`Translate(${x}, ${y})`);
